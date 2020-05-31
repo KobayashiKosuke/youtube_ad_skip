7a6b0ecfb4de3b44186faf30c9d6425278ed7a3e
kbyash <kbyash@amazon.com>
2020-05-31 18:33:09 +0900

    change type of variables

--- ad_skip.js
+++ ad_skip.js
@@ -1,20 +1,20 @@
-let ad_parent_node = document.querySelector(".video-ads.ytp-ad-module");
+const ad_parent_node = document.querySelector(".video-ads.ytp-ad-module");
 
-let callback = (mutationList, observer) => {
-  let ad_overlay_close_continer = document.querySelector(".ytp-ad-overlay-close-container");
+const callback = (mutationList, observer) => {
+  const ad_overlay_close_continer = document.querySelector(".ytp-ad-overlay-close-container");
   if (ad_overlay_close_continer) {
     ad_overlay_close_continer.click();
   }
-  let ad_overlay_skip_button = document.querySelector(".ytp-ad-skip-button-container");
+  const ad_overlay_skip_button = document.querySelector(".ytp-ad-skip-button-container");
   if (ad_overlay_skip_button){
     ad_overlay_skip_button.click();
 };
 
-let config = {
+const config = {
   attributes: true,
   childList: true,
   subtree: true
 };
 
-let observer = new MutationObserver(callback);
+const observer = new MutationObserver(callback);
 observer.observe(ad_parent_node, config);
